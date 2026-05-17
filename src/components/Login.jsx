198a0325c3e75108f36bb396067a573300568c0a
Hinkal Jain <hinkaljain@Hinkals-MacBook-Air.local>
2026-05-17 12:11:50 +0530

    added feed slice and user card

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,6 +8,7 @@ import { BASE_URL } from "../utils/const";
 const Login = () => {
   const [emailId, setEmailId] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -26,7 +27,7 @@ const Login = () => {
       dispatch(addUser(res.data));
       return navigate("/");
     } catch (error) {
-      console.error("Error occurred while logging in:", error);
+      setError(error?.response?.data || "Something went wrong");
     }
   };
 
@@ -52,7 +53,7 @@ const Login = () => {
           placeholder="Password"
           onChange={(e) => setPassword(e.target.value)}
         />
-
+        <p className="text-red-500">{error}</p>
         <button className="btn btn-neutral mt-4 w-full" onClick={handleLogin}>
           Login
         </button>
