60ce20dce65652403c760013c67f3c9b6219ee3a
Hinkal Jain <hinkaljain@Hinkals-MacBook-Air.local>
2026-05-18 23:58:13 +0530

    added sign up flow

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,6 +8,9 @@ import { BASE_URL } from "../utils/const";
 const Login = () => {
   const [emailId, setEmailId] = useState("");
   const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [isLoginForm, setIsLoginForm] = useState(true);
   const [error, setError] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -31,10 +34,49 @@ const Login = () => {
     }
   };
 
+  const handleSignUp = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/signup",
+        { firstName, lastName, emailId, password },
+        { withCredentials: true },
+      );
+      console.log(res);
+      dispatch(addUser(res.data.data));
+      return navigate("/profile");
+    } catch (err) {
+      setError(err?.response?.data || "Something went wrong");
+    }
+  };
+
   return (
     <div className="flex justify-center my-10">
       <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full max-w-md border p-6 shadow">
-        <legend className="fieldset-legend text-xl">Login</legend>
+        <legend className="fieldset-legend text-xl">
+          {isLoginForm ? "Login" : "Signup"}
+        </legend>
+
+        {!isLoginForm && (
+          <>
+            <label className="label">First Name</label>
+            <input
+              type="text"
+              value={firstName}
+              className="input w-full"
+              placeholder="First Name"
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+
+            <label className="label">Last Name</label>
+            <input
+              type="text"
+              value={lastName}
+              className="input w-full"
+              placeholder="Last Name"
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </>
+        )}
 
         <label className="label">Email</label>
         <input
@@ -54,9 +96,18 @@ const Login = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
         <p className="text-red-500">{error}</p>
-        <button className="btn btn-neutral mt-4 w-full" onClick={handleLogin}>
-          Login
+        <button
+          className="btn btn-neutral mt-4 w-full"
+          onClick={isLoginForm ? handleLogin : handleSignUp}
+        >
+          {isLoginForm ? "Login" : "Signup"}
         </button>
+        <p
+          className="cursor-pointer text-center mt-4 text-blue-500"
+          onClick={() => setIsLoginForm((value) => !value)}
+        >
+          {isLoginForm ? "New User? Signup Here" : "Existing User? Login Here"}
+        </p>
       </fieldset>
     </div>
   );
